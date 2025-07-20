166ca276b040a48b08425819930b88f51dc1fdb7
Sathvik1404 <160138146+Sathvik1404@users.noreply.github.com>
2025-07-20 15:19:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ import enrollRoute from './routes/user.enroll.js';
 
 import cors from 'cors';
 import dotenv from "dotenv";
+
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
-
 
 // Enable CORS
 app.use(
@@ -20,10 +20,15 @@ app.use(
     })
 );
 
+// ✅ Add this root route
+app.get("/", (req, res) => {
+    res.send("Tutone backend is up and running!");
+});
 
 app.use("/api", router);
 app.use("/api", enrollRoute);
+
 app.listen(PORT, async () => {
     console.log("Server is running on http://localhost:", PORT);
     await connectdb();
-})
+});
